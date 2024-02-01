4ba9ade46b6c32d677a2a24089386e18b4102f51
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-01 14:19:41 +0100

    create componenet

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,18 +1,44 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 
 const Login = () => {
   return (
-    <div className='container mt-5'>
-        <InputGroup className="mb-3 col-lg-9 d-flex justify-content-center">
-        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-        <Form.Control
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-        </InputGroup>
+    <div className='container mt-5 col-lg-5'>
+      <h3 className='m-2'>Login</h3>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="text" class="form-control" placeholder="Username" />
+          <label for="floatingInputGroup1">Username</label>
+        </div>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="text" class="form-control" placeholder="Email@exmple.com" />
+          <label for="floatingInputGroup1">Email</label>
+        </div>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="password" class="form-control" placeholder="Password" />
+          <label for="floatingInputGroup1">Password</label>
+        </div>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingInputGroup1" placeholder="Confirm_Password" />
+          <label for="floatingInputGroup1">confirm Password</label>
+        </div>
+      </div>
+
+      <button type="button" class="btn btn-primary col-3">Register</button>
+
     </div>
   )
 }
--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -2,8 +2,36 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-        <h1>Register</h1>
+    <div className='container mt-5 col-lg-5'>
+      <h3 className='m-2'>Welcom Back</h3>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="text" class="form-control" placeholder="Email@exmple.com" />
+          <label for="floatingInputGroup1">Email</label>
+        </div>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="password" class="form-control" placeholder="Password" />
+          <label for="floatingInputGroup1">Password</label>
+        </div>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text"></span>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingInputGroup1" placeholder="Confirm_Password" />
+          <label for="floatingInputGroup1">confirm Password</label>
+        </div>
+      </div>
+
+      <button type="button" class="btn btn-primary col-3">Register</button>
+
+
     </div>
   )
 }
